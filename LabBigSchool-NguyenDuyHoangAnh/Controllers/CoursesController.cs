e87fbc132c34cce8f129ed1db9a250f028cfed0f
hoanganh.nguyenduy02@hotmail.com <hoanganh.nguyenduy02@hotmail.com>
2022-06-01 16:13:53 +0700

    upload

--- LabBigSchool-NguyenDuyHoangAnh/Controllers/CoursesController.cs
+++ LabBigSchool-NguyenDuyHoangAnh/Controllers/CoursesController.cs
@@ -1,17 +1,63 @@
-﻿using System;
+﻿using LabBigSchool_NguyenDuyHoangAnh.Models;
+using LabBigSchool_NguyenDuyHoangAnh.Views.ViewModels;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
+
 
 namespace LabBigSchool_NguyenDuyHoangAnh.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
+
+        //
+
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var coure = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place,
+            };
+            _dbContext.courses.Add(coure);
+            _dbContext.SaveChanges();
+            return ReadirectToAction("Index", "Home");
+        }
+
+        private ActionResult ReadirectToAction(string v1, string v2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ LabBigSchool-NguyenDuyHoangAnh/ViewModels/CustomViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_NguyenDuyHoangAnh.ViewModels
+{
+    public class CustomViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+    }
+}
